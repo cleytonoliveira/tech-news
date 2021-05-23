7673d1e83257bd39a6e40a3c3583bfc8eab6d424
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-05-22 22:11:43 -0300

    feat(search): add search by date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,10 +1,11 @@
 from tech_news.database import search_news
 from requests.exceptions import HTTPError
+from datetime import datetime
 
 
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    """Busca pelo título"""
     try:
         news_by_title = search_news({
             "title": {"$regex": title, "$options": 'i'}
@@ -17,6 +18,17 @@ def search_by_title(title):
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    try:
+        format = "%Y-%m-%d"
+        datetime.strptime(date, format)
+        news_by_date = search_news({
+            "timestamp": {"$regex": date}
+        })
+        return [(new["title"], new["url"]) for new in news_by_date]
+    except ValueError:
+        raise ValueError("Data inválida")
+    except HTTPError:
+        return []
 
 
 # Requisito 8
